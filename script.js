aff5a11bea685a73d303a04240df32ce406c41c3
adnirJunior <adabjunior@hotmail.com>
2021-07-02 20:16:12 -0300

    Projeto próprio do zero..

--- script.js
+++ script.js
@@ -1,9 +1,232 @@
+    let pedTot01 = 0;
+    let pedTot02 = 0;
+    let pedTot03 = 0;
+    let pedTot04 = 0;
+    let pedTot05 = 0;
+    let pedTot06 = 0;
+    let pedTot07 = 0;
+    let pedTot08 = 0;
+    let pedTot09 = 0;
+    let pedTot10 = 0;
+    let pedTot11 = 0;
+    let pedTot12 = 0;
+    let pedTot13 = 0;
+    let pedTot14 = 0;
+    let pedTot15 = 0;
+    let pedTot16 = 0;
+    let pedTot17 = 0;
+    let pedTot18 = 0;
+    let pedTot19 = 0;
+    let pedTot20 = 0;
+
 function seupedido(){
-    var ped01 = document.getElementById("ped01").value;
-    var pedTot01 = document.getElementById("pedtot01").value;
+    
+    
+    let ped01 = document.getElementById("ped01").value;
+    let ped001 = parseInt(ped01);
 
-    pedTot01 += ped01;
+    
+    pedTot01 = ped001 + pedTot01;
+    
+    let valorTotal01 = pedTot01 * 9;
+    
+    document.getElementById("pedtot01").innerHTML = `${pedTot01}`;
+    document.getElementById("itemtotal01").innerHTML = `R$ ${valorTotal01.toFixed(2)} `;
 
-    alert(`Voce pediu ${pedTot01}`);
 
-}
+    let ped02 = document.getElementById("ped02").value;
+    let ped002 = parseInt(ped02);
+
+    pedTot02 += ped002;
+
+    let valorTotal02 = pedTot02 * 9;
+
+    document.getElementById("pedtot02").innerHTML = `${pedTot02}`;
+    document.getElementById("itemtotal02").innerHTML = `R$${valorTotal02.toFixed(2)}`
+
+
+    let ped03 = document.getElementById("ped03").value;
+    let ped003 = parseInt(ped03);
+
+    pedTot03 += ped003;
+    let valorTotal03 = pedTot03 * 11;
+
+    document.getElementById("pedtot03").innerHTML = `${pedTot03}`;
+    document.getElementById("itemtotal03").innerHTML = `R$${valorTotal03.toFixed(2)}`
+
+    let ped04 = document.getElementById("ped04").value
+    let ped004 = parseInt(ped04);
+
+    pedTot04 += ped004;
+    let valorTotal04 = pedTot04 * 25;
+
+    document.getElementById("pedtot04").innerHTML = `${pedTot04}`;
+    document.getElementById("itemtotal04").innerHTML = `R$${valorTotal04.toFixed(2)}`;
+
+    let ped05 = document.getElementById("ped05").value
+    let ped005 = parseInt(ped05);
+
+    pedTot05 += ped005;
+    let valorTotal05 = pedTot05 * 30;
+
+    document.getElementById("pedtot05").innerHTML = `${pedTot05}`;
+    document.getElementById("itemtotal05").innerHTML = `R$${valorTotal05.toFixed(2)}`;
+
+    let ped06 = document.getElementById("ped06").value;
+    let ped006 = parseInt(ped06);
+
+    pedTot06 += ped006;
+    let valorTotal06 = ped006 * 30;
+
+    document.getElementById("pedtot06").innerHTML = `${pedTot06}`;
+    document.getElementById("itemtotal06").innerHTML = `R$${valorTotal06.toFixed(2)}`
+
+    let ped07 = document.getElementById("ped07").value;
+    let ped007 = parseInt(ped07);
+
+    pedTot07 += ped007;
+    let valorTotal07 = ped007 * 8;
+
+    document.getElementById("pedtot07").innerHTML = `${pedTot07}`;
+    document.getElementById("itemtotal07").innerHTML = `R$${valorTotal07.toFixed(2)}`
+
+    
+    let ped08 = document.getElementById("ped08").value;
+    let ped008 = parseInt(ped08);
+
+    pedTot08 += ped008;
+    let valorTotal08 = ped008 * 13;
+
+    document.getElementById("pedtot08").innerHTML = `${pedTot08}`;
+    document.getElementById("itemtotal08").innerHTML = `R$${valorTotal08.toFixed(2)}`
+
+
+    let ped09 = document.getElementById("ped09").value;
+    let ped009 = parseInt(ped09);
+
+    pedTot09 += ped009;
+    let valorTotal09 = ped009 * 6;
+
+    document.getElementById("pedtot09").innerHTML = `${pedTot09}`;
+    document.getElementById("itemtotal09").innerHTML = `R$${valorTotal09.toFixed(2)}`
+
+    
+    let ped10 = document.getElementById("ped10").value;
+    let ped010 = parseInt(ped10);
+
+    pedTot10 += ped010;
+    let valorTotal10 = ped010 * 8;
+
+    document.getElementById("pedtot10").innerHTML = `${pedTot10}`;
+    document.getElementById("itemtotal10").innerHTML = `R$${valorTotal10.toFixed(2)}`
+
+    let ped11 = document.getElementById("ped11").value;
+    let ped011 = parseInt(ped11);
+
+    pedTot11 += ped011;
+    let valorTotal11 = ped011 * 30;
+
+    document.getElementById("pedtot11").innerHTML = `${pedTot11}`;
+    document.getElementById("itemtotal11").innerHTML = `R$${valorTotal11.toFixed(2)}`
+
+    let ped12 = document.getElementById("ped12").value;
+    let ped012 = parseInt(ped12);
+
+    pedTot12 += ped012;
+    let valorTotal12 = ped012 * 45;
+
+    document.getElementById("pedtot12").innerHTML = `${pedTot12}`;
+    document.getElementById("itemtotal12").innerHTML = `R$${valorTotal12.toFixed(2)}`
+
+    let ped13 = document.getElementById("ped13").value;
+    let ped013 = parseInt(ped13);
+
+    pedTot13 += ped013;
+    let valorTotal13 = ped013 * 50;
+
+    document.getElementById("pedtot13").innerHTML = `${pedTot13}`;
+    document.getElementById("itemtotal13").innerHTML = `R$${valorTotal13.toFixed(2)}`
+
+    let ped14 = document.getElementById("ped14").value;
+    let ped014 = parseInt(ped14);
+
+    pedTot14 += ped014;
+    let valorTotal14 = ped014 * 38;
+
+    document.getElementById("pedtot14").innerHTML = `${pedTot14}`;
+    document.getElementById("itemtotal14").innerHTML = `R$${valorTotal14.toFixed(2)}`
+
+    let ped15 = document.getElementById("ped15").value;
+    let ped015 = parseInt(ped15);
+
+    pedTot15 += ped015;
+    let valorTotal15 = ped015 * 110;
+
+    document.getElementById("pedtot15").innerHTML = `${pedTot15}`;
+    document.getElementById("itemtotal15").innerHTML = `R$${valorTotal15.toFixed(2)}`
+
+    let ped16 = document.getElementById("ped16").value;
+    let ped016 = parseInt(ped16);
+
+    pedTot16 += ped016;
+    let valorTotal16 = ped016 * 45;
+
+    document.getElementById("pedtot16").innerHTML = `${pedTot16}`;
+    document.getElementById("itemtotal16").innerHTML = `R$${valorTotal16.toFixed(2)}`
+
+    let ped17 = document.getElementById("ped17").value;
+    let ped017 = parseInt(ped17);
+
+    pedTot17 += ped017;
+    let valorTotal17 = ped017 * 38;
+
+    document.getElementById("pedtot17").innerHTML = `${pedTot17}`;
+    document.getElementById("itemtotal17").innerHTML = `R$${valorTotal17.toFixed(2)}`
+
+    let ped18 = document.getElementById("ped18").value;
+    let ped018 = parseInt(ped18);
+
+    pedTot18 += ped018;
+    let valorTotal18 = ped018 * 35;
+
+    document.getElementById("pedtot18").innerHTML = `${pedTot18}`;
+    document.getElementById("itemtotal18").innerHTML = `R$${valorTotal18.toFixed(2)}`
+
+    let ped19 = document.getElementById("ped19").value;
+    let ped019 = parseInt(ped19);
+
+    pedTot19 += ped019;
+    let valorTotal19 = ped019 * 45;
+
+    document.getElementById("pedtot19").innerHTML = `${pedTot19}`;
+    document.getElementById("itemtotal19").innerHTML = `R$${valorTotal19.toFixed(2)}`
+
+    let ped20 = document.getElementById("ped20").value;
+    let ped020 = parseInt(ped20);
+
+    pedTot20 += ped020;
+    let valorTotal20 = ped020 * 30;
+
+    document.getElementById("pedtot20").innerHTML = `${pedTot20}`;
+    document.getElementById("itemtotal20").innerHTML = `R$${valorTotal20.toFixed(2)}`
+
+
+
+        let contapetiscos = valorTotal20 + valorTotal19 + valorTotal18 + valorTotal17 + valorTotal16 + valorTotal15 + valorTotal14 + valorTotal13 + valorTotal12 + valorTotal11;
+        let contabebidas =  + valorTotal10 + valorTotal09 + valorTotal08 + valorTotal07 + valorTotal06 + valorTotal05 + valorTotal04 + valorTotal03 +  valorTotal02 + valorTotal01;
+    
+        let contaTotal = contapetiscos + contabebidas;
+        
+        alert(`O valor da conta atual é R$${contaTotal.toFixed(2)}`);
+} 
+
+    function vemgarcom(){
+        var img = document.createElement("IMG");
+     img.src = "./img/garcom.png";
+     img.style.width = "300px";
+     img.style.height = "300px";
+     img.style.position = "relative";
+     document.getElementById('wrap-garcom').appendChild(img);
+     
+    }
+
